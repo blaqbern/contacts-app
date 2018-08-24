4e6ca525f9e9ef9d2ce221a4c88ccf0c9fb55a52
blaqbern <burnicus8@gmail.com>
2018-08-23 22:28:19 -0400

    add redux devtools

--- src/store/configure-store.ts
+++ src/store/configure-store.ts
@@ -1,13 +1,17 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
+import { State } from '@store'
 import { cleanupMiddleware } from './middleware/cleanup-data'
 import { contacts as rootReducer } from './reducer'
 
-export function configureStore() {
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export function configureStore<State>() {
   const store = createStore(
     rootReducer,
-    applyMiddleware(thunk, cleanupMiddleware)
+    composeEnhancers(
+      applyMiddleware(thunk, cleanupMiddleware)
+    )
   )
 
   if ((module as any).hot) (module as any).hot.accept('./reducer', () =>
